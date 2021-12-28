1ec28e340335c4da829e674f5847b3aa01ce0b5d
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2021-12-28 20:25:18 +0500

    Add files via upload

---
+++ FixHP/Config.cs
@@ -0,0 +1,38 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FixHP
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "FixHP";
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("Dictionary of all classes: ")]
+        public Dictionary<RoleType, int> ConfigMaxpHp { get; set; } = new Dictionary<RoleType, int>
+        {
+            [RoleType.ChaosConscript] = 120,
+            [RoleType.ChaosMarauder] = 120,
+            [RoleType.ChaosRepressor] = 120,
+            [RoleType.ChaosRifleman] = 120,
+            [RoleType.ClassD] = 100,
+            [RoleType.FacilityGuard] = 100,
+            [RoleType.NtfCaptain] = 150,
+            [RoleType.NtfPrivate] = 100,
+            [RoleType.NtfSergeant] = 120,
+            [RoleType.NtfSpecialist] = 120,
+            [RoleType.Scientist] = 100,
+            [RoleType.Scp049] = 2500,
+            [RoleType.Scp0492] = 500,
+            [RoleType.Scp096] = 2000,
+            [RoleType.Tutorial] = 100,
+            [RoleType.Scp106] = 850,
+            [RoleType.Scp173] = 3000,
+            [RoleType.Scp93953] = 1800,
+            [RoleType.Scp93989] = 1800,
+            [RoleType.Tutorial] = 100
+        };
+    }
+}
---
+++ FixHP/FixHP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MEC;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Events;
+
+namespace FixHP
+{
+    public class FixHP : Plugin
+    {
+        public override string Developer => "KoToXleB#4663";
+        public override string Name => "FixHP";
+        public override Version Version => new Version(1, 0, 0);
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public Config CustomConfig { get; private set; }
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.RoleChange += OnRoleChange;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Player.RoleChange += OnRoleChange;
+        }
+        public void OnRoleChange(RoleChangeEvent ev)
+        {
+            if (CustomConfig.ConfigMaxpHp.TryGetValue(ev.NewRole, out int ConfigMaxpHp))
+            {
+                Timing.CallDelayed(0.1f, () => {
+                    ev.Player.MaxHp = ConfigMaxpHp;
+                    ev.Player.Hp = ConfigMaxpHp;
+                });
+            }
+        }
+    }
+}
